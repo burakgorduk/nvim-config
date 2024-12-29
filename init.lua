41649661e3479dd3cc72df9c8e507ac436b22f6b
burakgorduk <61919131+burakgorduk@users.noreply.github.com>
2024-12-29 20:25:05 +0000

    Fix options

--- init.lua
+++ init.lua
@@ -1,5 +1,3 @@
-require("config.options")
 require("config.lazy")
 require("config.mappings")
-vim.cmd.colorscheme "kanagawa"
-vim.cmd.set "relativenumber"
+require("config.options")
--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,9 @@
-vim.expandtab = true
-vim.shiftwidth = 2
-vim.smartindent = true
-vim.tabstop = 2
-vim.softtabstop = 2
-vim.g.mapleader = " "
+vim.cmd("colorscheme kanagawa")
+vim.cmd("set relativenumber")
+vim.cmd("set expandtab")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set smartindent")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
 
+vim.g.mapleader = " "

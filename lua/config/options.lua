d6634e03e5ddb744f4df4e5e3003c36da4af8e73
burakgorduk <61919131+burakgorduk@users.noreply.github.com>
2025-01-01 23:04:51 +0000

    Show both relative and absolute numbers

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,5 +1,4 @@
 vim.cmd("colorscheme kanagawa")
-vim.cmd("set relativenumber")
 vim.cmd("set expandtab")
 vim.cmd("set shiftwidth=2")
 vim.cmd("set smartindent")
@@ -7,3 +6,7 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 
 vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.statuscolumn = "%s %l %r "

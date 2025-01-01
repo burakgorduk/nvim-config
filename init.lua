3a69932e9e4cececb2eed61ae77d443ed511de44
burakgorduk <61919131+burakgorduk@users.noreply.github.com>
2025-01-01 16:38:50 +0000

    Add trailing space trim

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 require("config.lazy")
 require("config.mappings")
 require("config.options")
+
+vim.api.nvim_create_autocmd({ "BufWritePre" }, {
+  pattern = { "*" },
+  command = [[%s/\s\+$//e]],
+})
